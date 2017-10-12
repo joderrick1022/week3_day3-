3e4e91c8cccb2e20fd82e4ee4f740d7df68533c6
joderrick1022 <joderrick1022>
2017-10-12 14:31:41 -0400

    surgeon class made

---
+++ week3_day3/Surgeon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3_day3
+{
+    class Surgeon:Doctor
+    {
+
+        public Surgeon()
+        {
+
+        }
+
+        public Surgeon(string employeeName,int employeeNumber,string department, int numberOfPatience)
+        {
+            this.employeeName = employeeName;
+            this.employeeNumber = employeeNumber;
+            this.department = department;
+            this.numberOfPatience = numberOfPatience;
+        }
+    }
+}
